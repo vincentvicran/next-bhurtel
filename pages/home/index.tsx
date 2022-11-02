819683730f4fb8f8532fca2706126b3cf8806428
Binay shrestha <binayshrestha798@gmail.com>
2022-11-02 16:10:02 +0545

    refactor: minor refactor in homepage

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,22 +1,9 @@
-import {DescriptionCard} from 'components/descriptionCard'
 import {MainCarousel} from '../../common/carousel'
 
 const HomePage = () => {
   return (
     <div>
       <MainCarousel />
-
-      <div style={{width: 500, padding: 50}}>
-        <DescriptionCard
-          imgUrl={
-            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/carbon-fiber-shelby-mustang-1600685276.jpg?crop=0.9988636363636364xw:1xh;center,top&resize=480:*'
-          }
-          title="What are the major Construction Accident reports in NYC?"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa eu, aliquam gravida quis. Convallis urna amet aenean mi habitant a, non eu. A elementum leo ultrices morbi malesuada. Eget enim rhoncus aenean tempus mauris nibh"
-          author="binay"
-          date={new Date()}
-        />
-      </div>
     </div>
   )
 }
