9d6ff0bae7fd3ca55630b26e939ab140b65f994e
ankit417 <acharya.ankit417@gmail.com>
2022-11-04 11:49:06 +0545

    Comp wrapper fixes

--- common/compWrapper/compwrapper.styles.ts
+++ common/compWrapper/compwrapper.styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import Theme from 'theme'
 
 export const CompWrapperContainer = styled.div`
-width: ${Theme.width.$default}
-margin: 0px auto;
-
+  width: ${Theme.width.$default};
+  margin: 0px auto;
 `
