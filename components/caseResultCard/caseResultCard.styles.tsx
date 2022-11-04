055d921ee4d32d5e6d08653560b65c0854e83b16
Binay shrestha <binayshrestha798@gmail.com>
2022-11-04 11:32:53 +0545

    feat: case result card created

---
+++ components/caseResultCard/caseResultCard.styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import Theme from 'theme'
+
+export const CaseResCard = styled.div`
+  border: 1px solid ${Theme.colors.$borderPrimary};
+  border-radius: ${Theme.radius.$default};
+  padding: ${Theme.space.$4};
+  width: 100%;
+`
