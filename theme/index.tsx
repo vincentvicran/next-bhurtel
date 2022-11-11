dd7cc2fc145df614f3114116ff6611b5e7c7d368
Baivab Bidari <baivabbidari987@gmail.com>
2022-11-11 11:27:37 +0545

    feat:custom scrollbar ui

--- theme/index.tsx
+++ theme/index.tsx
@@ -155,6 +155,17 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+::-webkit-scrollbar {
+  width: 10px;
+  height: 3px;
+}
+::-webkit-scrollbar-track {
+  background-color: #f1f1f1;
+}
+::-webkit-scrollbar-thumb {
+  background-color:rgb(179, 179, 179);
+  border-radius:${Theme.radius.$default};
+}
     }
 
     :root {
