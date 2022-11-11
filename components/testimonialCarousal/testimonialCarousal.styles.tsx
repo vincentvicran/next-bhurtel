162dcef3aebbbd188a72c225cbb45590fe74a886
Baivab Bidari <baivabbidari987@gmail.com>
2022-11-11 11:35:51 +0545

    fix :removed active border on testimonial

--- components/testimonialCarousal/testimonialCarousal.styles.tsx
+++ components/testimonialCarousal/testimonialCarousal.styles.tsx
@@ -20,8 +20,6 @@ export const CarousalContainer = styled.div`
     > div
     > div {
     transition: all 0.8s ease-in-out;
-    border: 1px solid ${Theme.colors.$primary};
-    border-radius: 0px;
   }
 
   .slick-dots > li {
