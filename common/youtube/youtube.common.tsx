61a27e94b443559fa18799a80a7c61929f32ef34
vincentvicran <vvicran@gmail.com>
2022-11-14 16:11:34 +0545

    feat: Video Id Link Regex Matcher Added

--- common/youtube/youtube.common.tsx
+++ common/youtube/youtube.common.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import YouTube, {YouTubeProps} from 'react-youtube'
 import {YoutubeContainer} from './youtube.style'
 
@@ -19,9 +19,17 @@ export const Youtube = ({videoId}: YoutubeUserProps) => {
     }
   }
 
+  const splittedVideoId = useMemo(() => {
+    const videoIdRegex =
+      /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/
+    const newVideoId = videoId.match(videoIdRegex)?.toString()
+    console.log('newVideoId: ', videoId, newVideoId)
+    return newVideoId
+  }, [videoId])
+
   return (
     <YoutubeContainer>
-      <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} />
+      <YouTube videoId={splittedVideoId} opts={opts} onReady={onPlayerReady} />
     </YoutubeContainer>
   )
 }
